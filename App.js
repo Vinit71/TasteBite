1e7086e82223906c63981583b58cff472b35124d
Vinit <vinitsk99@gmail.com>
2024-08-10 13:27:52 +0530

    Setup react using CDN links and understand Parcel Bundler

---
+++ App.js
@@ -0,0 +1,38 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hello from the React")
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// create this structure using React.createElement();
+{/* <div id="parent">
+    <div id="child1">
+        <h1>heading1</h1>
+        <h1>heading2</h1>
+    </div>
+    <div id="child2">
+        <h1>heading1</h1>
+        <h1>heading2</h1>
+    </div>
+</div> */}
+
+const parent = React.createElement("div", {id: "parent"}, 
+    [
+        React.createElement("div", {id: "child1"},
+            [
+                React.createElement("h1", {}, "heading1"), React.createElement("h1", {}, "heading2")
+            ]), 
+            React.createElement("div", {id: "child2"},
+            [
+                React.createElement("h1", {}, "heading1"), React.createElement("h1", {}, "heading2"),
+            ]
+        ) 
+    ]
+);
+//this code looks very ugly so we will use 'JSX' to make our life easy.
+//still this is the core of React, JSX just help us to do these things easily
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent);    //plain js object
+
+root.render(parent);
